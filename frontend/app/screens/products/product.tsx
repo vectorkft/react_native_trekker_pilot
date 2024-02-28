33a619e720ed8ab36cdada47c57dc8a3bba3e9af
Urbaniczki Lajos <urbaniczki.lajos@vector.hu>
2024-02-28 12:03:44 +0100

    kamera bezárási lehetőség

--- frontend/app/screens/products/product.tsx
+++ frontend/app/screens/products/product.tsx
@@ -1,14 +1,16 @@
-import React, {JSX} from "react";
-import {View, TextInput, Alert, Keyboard} from "react-native";
+import React, {JSX, useEffect, useState} from "react";
+import {View, TextInput, Alert, Keyboard, Text, StyleSheet, Button} from "react-native";
 import {ProductsService} from '../../services/products.service';
-import {articleStyles} from "../../styles/products.stylesheet";
+import {articleStyles, camera} from "../../styles/products.stylesheet";
 import {parseResponseMessages} from "../../../../shared/services/zod-dto.service";
 import {ZArticleDTOOutput2} from "../../../../shared/dto/article.dto";
-import CardComponentSuccess from "../../components/card/card-component";
+import { BarCodeScanner } from 'expo-barcode-scanner';
 import CardComponentNotFound from "../../components/card/card-component-not-found";
 import ButtonComponent from "../../components/button/button-component";
 import DataTable from "../../components/table/data-table";
-// import Sound from 'react-native-sound';
+import {Ionicons} from "@expo/vector-icons";
+import FontAwesome from 'react-native-vector-icons/FontAwesome';
+
 
 const Product = (): JSX.Element => {
     const [searchQuery, setSearchQuery] = React.useState('');
@@ -16,42 +18,35 @@ const Product = (): JSX.Element => {
     const [value, setValue] = React.useState(0);
     const timeout = React.useRef(null);
     const [result, setResult] = React.useState<ZArticleDTOOutput2|false|Response>();
+    const [scanned, setScanned] = useState(true);
+    const [text, setText] = useState('Not yet scanned')
+    const [hasPermission, setHasPermission] = useState(null);
+    const [isCameraActive, setIsCameraActive] = useState(false);
 
-    // const sound = new Sound('frontend/app/assets/beep-07a.mp3', Sound.MAIN_BUNDLE, (error) => {
-    //     if (error) {
-    //         console.log('Failed to load the sound', error);
-    //         return;
-    //     }
-    // });
-    // async function handleFormSubmit() {
-    //     const eanNumber = Number(searchQuery);
-    //
-    //     if (isNaN(eanNumber)) {
-    //         Alert.alert('Hiba', 'Kérjük, adjon meg egy érvényes számot.');
-    //     } else {
-    //         try {
-    //             const response = await ProductsService.getArticlesByEAN({ eankod: eanNumber });
-    //             setResult(response);
-    //             setSearchQueryState(searchQuery);
-    //
-    //             if (globalFlagHelperFailed && response && 'status' in response) {
-    //                 const msg = await parseResponseMessages(response);
-    //                 Alert.alert('Hiba!', msg);
-    //             }
-    //
-    //
-    //             Keyboard.dismiss();
-    //             // sound.play((success) => {
-    //             //     if (!success) {
-    //             //         console.log('Sound did not play successfully');
-    //             //     }
-    //             // });
-    //
-    //         } catch (error: any) {
-    //             console.log('Hiba történt', error);
-    //         }
-    //     }
-    // }
+    useEffect(() => {
+        const getBarCodeScannerPermissions = async () => {
+            const { status } = await BarCodeScanner.requestPermissionsAsync();
+            setHasPermission(status === 'granted');
+        };
+
+        getBarCodeScannerPermissions().catch(error => console.log(error));
+    }, []);
+
+    const handleBarCodeScanned = ({ type, data }) => {
+        setScanned(true);
+        onChangeHandler(data)
+        setIsCameraActive(false);
+
+    };
+
+    if (hasPermission === null) {
+        return <Text>Requesting for camera permission</Text>;
+    }
+    if (hasPermission === false) {
+        return <Text>No access to camera</Text>;
+    }
+
+
     const onChangeHandler = (value: number) => {
         clearTimeout(timeout.current);
         setValue(value);
@@ -74,11 +69,7 @@ const Product = (): JSX.Element => {
 
 
                     Keyboard.dismiss();
-                    // sound.play((success) => {
-                    //     if (!success) {
-                    //         console.log('Sound did not play successfully');
-                    //     }
-                    // });
+
 
                 } catch (error: any) {
                     console.log('Hiba történt', error);
@@ -90,22 +81,29 @@ const Product = (): JSX.Element => {
 
     return (
         <View style={articleStyles.container}>
+            {/*<FontAwesome name="rocket" size={30} color="#900" />*/}
+            {isCameraActive && (
+                <View style={camera.container}>
+                    <BarCodeScanner
+                        onBarCodeScanned={scanned ? undefined : handleBarCodeScanned}
+                        style={StyleSheet.absoluteFillObject} />
+                    <View style={{ position: 'absolute', top: 0, right: 0, padding: 20 }}>
+                        <Ionicons name="md-close" size={50} color="white" onPress={() => {setIsCameraActive(false);setScanned(true)}} />
+                    </View>
+                </View>
+            )}
             <TextInput
                 style={articleStyles.input}
                 onChangeText={ (value: any) => {onChangeHandler(value)
                     setSearchQuery(searchQuery)
                 } }
-                // onChangeText={async (text) => {
-                //     setSearchQuery(text);
-                //
-                //
-                // }}
                 value={searchQuery}
                 placeholder="Keresés..."
                 keyboardType="numeric"
                 autoFocus
-                // onFocus = {()=> Keyboard.dismiss()}
+                onFocus = {()=> Keyboard.dismiss()}
             />
+            {scanned && <ButtonComponent label={'Camera'} enabled={true} onClick={() => { setScanned(false); setIsCameraActive(true); }} />}
             <ButtonComponent label={"Keresés"} enabled={true} onClick={()=>onChangeHandler}/>
             {result && 'cikkszam' in result && (
                 <View>
