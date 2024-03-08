b15c6323da3df0a11744e15c2643588ccae4ee76
Urbaniczki Lajos <urbaniczki.lajos@vector.hu>
2024-03-08 09:45:06 +0100

    A cikk service-t zodosítottam, egyszerűsítettem, igyekeztem átláthatóbbá tenni.

--- backend/services/cikkService.js
+++ backend/services/cikkService.js
@@ -9,97 +9,61 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
     });
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.getCikkByEanKod2 = exports.getCikkByEanKod = exports.getCikkByCikkszam = void 0;
+exports.getCikkByEanKod = exports.getCikkByCikkszam = void 0;
 const client_1 = require("@prisma/client");
-const cikkNotFoundDTO_1 = require("../dto/cikkNotFoundDTO");
 const article_dto_1 = require("../../shared/dto/article.dto");
 const zod_dto_service_1 = require("../../shared/services/zod-dto.service");
 const prisma = new client_1.PrismaClient();
 function getCikkByCikkszam(cikkszam) {
     return __awaiter(this, void 0, void 0, function* () {
-        try {
-            const cikk = yield prisma.cikk.findFirst({
-                where: {
-                    cikkszam: cikkszam
-                }
-            });
-            if (!cikk || !cikk.cikkszam || !cikk.cikknev || !cikk.eankod) {
-                return "Not found";
+        const cikk = yield prisma.cikk.findFirst({
+            where: {
+                cikkszam: cikkszam.cikkszam
             }
-            const body = yield (0, zod_dto_service_1.zParse)(article_dto_1.ArticleDTOOutput, cikk);
-            return body;
+        });
+        if (!cikk || !cikk.cikkszam || !cikk.cikknev || !cikk.eankod) {
+            return "Not found";
         }
-        catch (err) {
-            console.log(err);
-            throw err;
-        }
+        return (0, zod_dto_service_1.zParse)(article_dto_1.ArticleDTOOutput, cikk);
     });
 }
 exports.getCikkByCikkszam = getCikkByCikkszam;
 function getCikkByEanKod(eankod) {
     return __awaiter(this, void 0, void 0, function* () {
         try {
-            const cikk = yield prisma.cikk.findFirst({
+            const cikk = yield prisma.cikk.findMany({
                 where: {
-                    eankod: eankod
+                    eankod: eankod.eankod
                 }
             });
-            if (!cikk || !cikk.cikkszam || !cikk.cikknev || !cikk.eankod) {
-                return new cikkNotFoundDTO_1.CikkNotFoundDTO('Not found', eankod);
+            if (cikk.length === 0) {
+                return false;
             }
-            console.log(typeof eankod);
-            const body = yield (0, zod_dto_service_1.zParse)(article_dto_1.ArticleDTOOutput, cikk);
-            return body;
+            const result = {
+                data: cikk.flatMap((cikkElement) => [
+                    article_dto_1.ArticleDataOutput.parse({
+                        key: 'cikkszam',
+                        title: 'Cikkszám',
+                        value: cikkElement.cikkszam.toString(),
+                    }),
+                    article_dto_1.ArticleDataOutput.parse({
+                        key: 'cikknev',
+                        title: 'Cikknév',
+                        value: cikkElement.cikknev.toString(),
+                    }),
+                    article_dto_1.ArticleDataOutput.parse({
+                        key: 'eankod',
+                        title: 'EAN Kód',
+                        value: cikkElement.eankod.toString(),
+                    }),
+                ]),
+                count: cikk.length
+            };
+            return article_dto_1.ArticleListOutput.parse(result);
         }
         catch (err) {
-            console.log(err);
             throw err;
         }
     });
 }
 exports.getCikkByEanKod = getCikkByEanKod;
-function getCikkByEanKod2(eankod) {
-    return __awaiter(this, void 0, void 0, function* () {
-        try {
-            const cikk = yield prisma.cikk.findMany({
-                where: {
-                    eankod: eankod
-                }
-            });
-            if (cikk.length === 0) {
-                return new cikkNotFoundDTO_1.CikkNotFoundDTO('Not found', eankod);
-            }
-            const result = {
-                data: [],
-                count: 0
-            };
-            for (let i = 0; i < cikk.length; i++) {
-                const temp = [
-                    article_dto_1.ArticleDataOutput.parse({
-                        key: 'cikkszam',
-                        title: 'Cikkszám',
-                        value: cikk[i].cikkszam.toString(),
-                    }),
-                    article_dto_1.ArticleDataOutput.parse({
-                        key: 'cikknev',
-                        title: 'Cikknév',
-                        value: cikk[i].cikknev.toString(),
-                    }),
-                    article_dto_1.ArticleDataOutput.parse({
-                        key: 'eankod',
-                        title: 'EAN Kód',
-                        value: cikk[i].eankod.toString(),
-                    }),
-                ];
-                result.data.push(...temp);
-                result.count += 1;
-            }
-            return article_dto_1.ArticleListOutput.parse(result);
-        }
-        catch (err) {
-            console.log(err);
-            throw err;
-        }
-    });
-}
-exports.getCikkByEanKod2 = getCikkByEanKod2;
