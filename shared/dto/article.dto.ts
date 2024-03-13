6d8d744483e099c83817079d42f5dcc046c31d80
vectorkovacssandor <kovacs.sandor@vector.hu>
2024-03-13 13:08:21 +0100

    updated again

--- shared/dto/article.dto.ts
+++ shared/dto/article.dto.ts
@@ -1,45 +1,34 @@
 import {z} from "zod";
 import { ean } from 'luhn-validation';
-export const ArticleDTOOutput = z.object({
+export const ProductDTOOutput = z.object({
     cikkszam:z.number(),
     cikknev:z.string(),
     eankod: z.bigint(),
 });
 
-export const ArticleDataDTO = z.object({
+export const ProductDataOutput = z.object({
     key: z.string(),
     title: z.string(),
     value: z.string(),
 });
 
-export const ArticleDataOutput = z.object({
-    key: z.string(),
-    title: z.string(),
-    value: z.string(),
-});
-
-
-
-export const cikkEANSchemaInput = z.object({
-
+export const ProductEANSchemaInput = z.object({
     eankod: z.number().refine(value => value.toString().length === 13, 'Az EAN kód pontosan 13 karakter hosszú kell legyen!.')
         .refine(value => ean(value),'Nem valid EAN kód'),
 });
-export const cikkSzamSchemaInput = z.object({
 
-    cikkszam: z.number(),
+export const ProductNumberSchemaInput = z.object({
+    cikkszam: z.number().refine(value => typeof value === 'number', 'Nem számot adtál meg!'),
 });
 
-
-export const ArticleListOutput = z.object({
-    data: z.array(ArticleDataOutput),
+export const ProductListOutput = z.object({
+    data: z.array(ProductDataOutput),
     count: z.number(),
 });
 
-export type ZArticleDTOOutputData = z.infer<typeof ArticleDataDTO>
-export type ZcikkEANSchemaInput = z.infer<typeof cikkEANSchemaInput>
-export type ZcikkSzamSchemaInput = z.infer<typeof cikkSzamSchemaInput>
-export type ZArticleListOutput = z.infer<typeof ArticleListOutput>
+export type ZProductEANSchemaInput = z.infer<typeof ProductEANSchemaInput>
+export type ZProductNumberSchemaInput = z.infer<typeof ProductNumberSchemaInput>
+export type ZProductListOutput = z.infer<typeof ProductListOutput>
 
 
 
