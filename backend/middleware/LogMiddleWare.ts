fffce73397f9231a831ccd02bfba319c2f555c8c
Urbaniczki Lajos <urbaniczki.lajos@vector.hu>
2024-03-20 15:11:44 +0100

    a request logger-re raktam chalk-ot hogy szinesen irja ki a terminálra(nagyon vagany)

--- backend/middleware/LogMiddleWare.ts
+++ backend/middleware/LogMiddleWare.ts
@@ -1,14 +1,18 @@
 import {NextFunction, Request, Response} from 'express';
 
+import Chalk from 'chalk';
 
 export function Logger(req: Request, res: Response, next: NextFunction){
     const timestamp = new Date().toISOString();
     const { method, url, ip, body } = req;
-    console.log(`
+    console.log(Chalk.cyan(`
+          Incoming Request
+          
           ${timestamp} 
           ${method} ${url} 
           ${ip}
-          ${JSON.stringify(body)}`);
+          ${JSON.stringify(body)}`));
+
     next();
 }
 
