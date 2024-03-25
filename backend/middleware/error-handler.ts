16da31be413a2f1afd8ee1d12d5c4b471da2dd84
Urbaniczki Lajos <urbaniczki.lajos@vector.hu>
2024-03-25 09:47:33 +0100

    csinaltam uj zoderrorparse-ot

--- backend/middleware/error-handler.ts
+++ backend/middleware/error-handler.ts
@@ -1,4 +1,4 @@
-import { ZodDTO } from "../dto/zodDTO";
+
 import {ZodError} from "zod";
 import {NextFunction, Request, Response} from 'express';
 import {
@@ -6,9 +6,10 @@ import {
     HTTP_STATUS_INTERNAL_SERVER_ERROR, HTTP_STATUS_NOT_FOUND,
     HTTP_STATUS_UNAUTHORIZED
 } from "../constants/http-status-codes";
+import {parseZodError_backend} from "../../shared/services/zod";
 
 
-export function handleErrors(err: Error, _req: Request, res: Response, _next: NextFunction) {
+export async function handleErrors(err: Error, _req: Request, res: Response, _next: NextFunction) {
     const statusMessageMapping: Record<string, {status: number, message: string}> = {
         PrismaClientRustPanicError: {status: HTTP_STATUS_UNAUTHORIZED, message: 'Invalid username or password'},
         PrismaClientInitializationError: {status: HTTP_STATUS_INTERNAL_SERVER_ERROR, message: 'Cannot connect to the database'},
@@ -23,7 +24,8 @@ export function handleErrors(err: Error, _req: Request, res: Response, _next: Ne
     if (!statusMessage) { return res.status(HTTP_STATUS_INTERNAL_SERVER_ERROR).json({message: 'Unexpected error'}); }
 
     if (err instanceof ZodError){
-        return res.status(statusMessage.status).json({error: ZodDTO.fromZodError(err)});
+        //return res.status(statusMessage.status).json({error: ZodDTO.fromZodError(err)});
+        return res.status(statusMessage.status).json(await parseZodError_backend(err));
     }
 
     return res.status(statusMessage.status).json({message: statusMessage.message || err.message});
